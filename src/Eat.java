ea6f8dd1c0abe0d24ad36cfe11d349982735d7f5
deepthivempati <deepthi2135@gmail.com>
2022-11-01 15:04:51 +0530

    simple method call

--- src/Eat.java
+++ src/Eat.java
@@ -1,5 +1,12 @@
-public class Main {
+public class Eat {
+    void junkFood(){
+        System.out.println("No junkFood!");
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Eat junk = new Eat();
+        junk.junkFood();
     }
 }
+
+// cannot call Eat.junkFood since non-static methods cannot be referred from static
+// so object of the class is created and call the non-static method(ex: Eat)
